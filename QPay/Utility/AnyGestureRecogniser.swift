8e54ff916d924c7b6006dfcdc5ff62586d0f6e5c
md.yamin <myamin335@gmail.com>
2020-08-13 17:06:04 +0600

    Anygesture

---
+++ QPay/Utility/AnyGestureRecogniser.swift
@@ -0,0 +1,28 @@
+//
+//  AnyGestureRecogniser.swift
+//  QPay
+//
+//  Created by Md. Yamin on 8/13/20.
+//  Copyright © 2020 QPay. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+class AnyGestureRecognizer: UIGestureRecognizer {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        //To prevent keyboard hide and show when switching from one textfield to another
+        if let textField = touches.first?.view, textField is UITextField {
+            state = .failed
+        } else {
+            state = .began
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+       state = .ended
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+        state = .cancelled
+    }
+}
