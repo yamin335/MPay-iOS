b33ff116a79f988a9f6c41693bdc1308b03714dc
md.yamin <myamin335@gmail.com>
2020-08-13 06:25:39 +0600

    Initial Commit

---
+++ QPay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QPay
+//
+//  Created by Md. Yamin on 8/13/20.
+//  Copyright © 2020 QPay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
